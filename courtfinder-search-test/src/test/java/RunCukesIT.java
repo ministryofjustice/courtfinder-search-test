c26a30e11d6afcfa45d78f1140a50f9d33d89063
milror00 <robmilroy@urbangraphicstudio.com>
2014-09-02 12:49:25 +0100

    remove dir

--- courtfinder-search-test/src/test/java/RunCukesIT.java
+++
@@ -1,9 +0,0 @@
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-
-@RunWith(Cucumber.class)
-@CucumberOptions( tags={"@todo","~@needtoclarify"},format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
-public class RunCukesIT {
-
-}
--- courtfinder-search-test/src/test/java/uk/gov/justice/digital/courtfinder/step/HtmlStepContext.java
+++
@@ -1,5 +0,0 @@
-package uk.gov.justice.digital.courtfinder.step;
-
-public class HtmlStepContext {
-
-}
