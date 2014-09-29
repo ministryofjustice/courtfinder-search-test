fc6703e76d4f8beedecffddab00d039114c025fc
milror00 <robmilroy@urbangraphicstudio.com>
2014-09-29 14:44:51 +0100

    maven properties

--- src/test/java/uk/gov/justice/digital/courtfinder/factories/FakeDataFactory.java
+++ src/test/java/uk/gov/justice/digital/courtfinder/factories/FakeDataFactory.java
@@ -5,8 +5,8 @@ public class FakeDataFactory {
 	
 	public static String getCourtFinderStartPageUrl(){
 		if (ConfigurationFactory.getEnvironment() == ConfigurationFactory.developmentEnvironment)
-		    return "http://54.72.152.89/";
-		return "http://127.0.0.1:8000/";
+		    return "http://127.0.0.1:8000/";
+		return "http://54.72.152.89/";
 	}
 	
 
